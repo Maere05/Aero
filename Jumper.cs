2d9f56e3cca700beca975e68fe082095653ca262
maere <maere@MARIUS-SURFACE>
2022-05-08 09:14:44 +0200

    Add project files.

---
+++ Jumper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Aero
+{
+    /*
+     *      Formulas:
+     * ---------------------
+     *      v = v0 + a*t
+     * 
+     *      a = f/m
+     * 
+     *      v = v0 + (f/m) * t
+     *      
+     *      
+     *      
+     *   AOA 30:
+     *    
+     *   Lift : 421
+     *   Drag : 262
+     *    
+     *    
+     * 
+     */
+
+    class Jumper
+    {
+        public Vector2 V, FG, FD, FL, Fres, Pos;
+
+        public float VelocityAngle = 0, AoA, time, mass;
+
+        float gravityConst = 9.81f;
+
+        float v0x,v0y, fx, fy, t, m;
+
+        static float LiftC = 421f / 900f;
+        static float DragC = 262f / 900f;
+
+
+
+        public string Init(float mass_ ,float time_,float StartVel)
+        {
+            mass = mass_;
+            time = time_;
+
+            v0x = MathF.Cos(VelocityAngle) * StartVel;     // Cosine function is in radians :(
+            v0y = -(MathF.Sin(VelocityAngle) * StartVel);
+
+            //Position
+            Pos = new Vector2(20f, 20f);
+
+            //Gravity Force
+            FG = new Vector2(0f, mass * (-9.81f));      
+
+            //Starting Velocity
+            V = new Vector2();
+
+            V.X =  MathF.Cos(VelocityAngle) * StartVel;     // Cosine function is in radians :(
+            V.Y = -(MathF.Sin(VelocityAngle) * StartVel);
+
+            //Lift Forces
+            FL = new Vector2();
+
+            FL.X = MathF.Sin(VelocityAngle) * (LiftC * V.LengthSquared());    // Mutliply with V^2            
+            FL.Y = MathF.Cos(VelocityAngle) * (LiftC * V.LengthSquared());
+            
+
+            //Drag Forces
+            FD = new Vector2();
+
+            FD.X = -(MathF.Cos(VelocityAngle) * (DragC * V.LengthSquared()));   // Multipy with V^2
+            FD.Y =  MathF.Sin(VelocityAngle) * (DragC * V.LengthSquared());
+
+            return "Pos:" + Pos.ToString() + "\nGravity" + FG.ToString() + "\nlift" + FL.ToString() + "\nDrag" + FD.ToString() + "\nVel" + V.ToString();
+        }
+
+        public Vector2 Timestep()
+        {
+
+
+            Fres = FG + FD + FL;
+
+            V.Y = v0y + (Fres.Y / mass) * time;
+            v0y = V.Y;
+
+            V.X = v0x + (Fres.X / mass) * time;
+            v0x = V.X;
+
+
+            Pos.X += V.X*time;      // m/s * s = m
+            Pos.Y -= V.Y*time;                          // minus because its an easy fix
+
+
+            VelocityAngle = MathF.Atan(V.Y / V.X);
+
+            //Lift Forces
+
+            FL.X = MathF.Sin(VelocityAngle) * (LiftC * V.LengthSquared());    // Mutliply with V^2            
+            FL.Y = MathF.Cos(VelocityAngle) * (LiftC * V.LengthSquared());
+
+
+            //Drag Forces
+
+            FD.X = -MathF.Cos(VelocityAngle) * (DragC * V.LengthSquared());   // Multipy with V^2
+            FD.Y = MathF.Sin(VelocityAngle) * (DragC * V.LengthSquared());
+
+            float vel = V.Length();
+
+            return Pos;
+        }
+    }    
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+//using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Numerics;
+
+
+namespace Aero
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>e
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        static int h = 500;
+        static int w = 500;
+
+        float zoom = 3f;
+        float yOffset = 0;
+        float xOffset = 0;  
+
+        Jumper jumper;
+
+        Bitmap bmp = new Bitmap(h, w);
+        Graphics g;
+        Pen pen = new Pen(Color.White, 2.0f);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initBitmap();
+
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            jumper = new Jumper();
+            jumper.V = Vector2.Zero;
+            jumper.Pos.X = 0;
+            jumper.Pos.Y = 0;
+            jumper.VelocityAngle = 0;
+
+            g = Graphics.FromImage(bmp);
+
+            Path path = new Path();
+
+
+            g.Clear(Color.Black);
+            //g.DrawBezier(pen, 0, 0, 300, 0, 200, 500, 500, 500);
+
+
+
+
+            txtOut.Text = jumper.Init((float)Sld1.Value,0.1f, 29f) + "----------------\n\n";
+
+
+            for (int i = 0; i < 45; i++)
+            {
+                points();
+            }
+
+
+
+
+            IntPtr hBitmap = bmp.GetHbitmap();
+
+            //what the heck is this statement
+            viewport.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap
+                (hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        private void initBitmap()
+        {
+
+        }
+
+        private void points()
+        {
+            Vector2 p1 = jumper.Timestep();
+
+            xOffset = 0;
+            yOffset = 0;
+
+            //txtOut.Text += jumper.V.Length().ToString() + " ;";
+            //txtOut.Text += jumper.FL.Length().ToString() + " ;";
+            //txtOut.Text += jumper.FD.Length().ToString() + "\n";
+
+            txtOut.Text += (jumper.VelocityAngle / MathF.PI*180f).ToString() + "\n";
+
+
+
+
+
+
+            g.DrawLine(pen, (int)p1.X*zoom + xOffset, (int)p1.Y*zoom + yOffset, (int)p1.X*zoom + 3 + xOffset, (int)p1.Y*zoom + 3 + yOffset);
+
+        }
+
+
+    }
+}
